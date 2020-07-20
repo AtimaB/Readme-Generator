086530ef54a171a27597ec2d51b32f5e3578a83f
AtimaB <nickzehn10@hotmail.com>
2020-07-20 15:59:19 -0600

    create generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,64 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+let generateMarkdown = require('./utils/generateMarkdown');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter the description of your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter the installation instructions of your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "please enter the usage information of your project?"
+    },
+    {
+        type: "choice",
+        name: "license",
+        message: "Please choose your license?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What is the contributing guideline of your project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide tests instructions of your project?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "please enter your Email address."
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub Username."
+    },
+    {
+        type: "input",
+        name: "question",
+        message: "Please provide the insrtuctions on how to reach you."
+    },
 ];
 
 // function to write README file
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+    ## Description
+    ${data.description}
+    ## Installation
+    ${data.installation}
+    ## Table of Contents 
+      * [Installation](#installation)
+      * [Usage](#usage)
+      * [Credits](#credits)
+      * [License](#license)
+      * [Tests](#tests)
+      * [Questions](#questions)
+      * [Contributing](#contributing)
+      * [GitHub](#github)
+      * [Email](#email) 
+    ## Usage Information
+    ${data.usage}
+    ## License
+    ${data.licens}
+    ## Contributing Guidelines
+    ${data.contributing}
+    ## Tests Instructions
+    ${data.tests}
+    ## Email Address
+    ${data.email}
+    ## GitHub Username
+    ${data.github}
+    ### Contact Instructions
+    ${data.question}
 `;
 }
 
