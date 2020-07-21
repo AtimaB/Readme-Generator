73650f5e2fb5998da6879e06a338cf123439553e
AtimaB <nickzehn10@hotmail.com>
2020-07-21 14:44:52 -0600

    create readme.md  file

--- Develop/index.js
+++ Develop/index.js
@@ -1,74 +1,89 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-let generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
 
 // array of questions for user
-const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the name of your project title?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please enter the description of your project?"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Please enter the installation instructions of your project?"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "please enter the usage information of your project?"
-    },
-    {
-        type: "choice",
-        name: "license",
-        message: "Please choose your license?"
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "What is the contributing guideline of your project?"
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Please provide tests instructions of your project?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "please enter your Email address."
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "Please enter your GitHub Username."
-    },
-    {
-        type: "input",
-        name: "question",
-        message: "Please provide the insrtuctions on how to reach you."
-    },
-];
+function promptUser() {
+    return inquirer.prompt([ 
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of your project title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter the description of your project?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide the installation instructions of your project?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "please enter the usage information of your project?"
+        },
+        {
+            type: "choice",
+            name: "license",
+            message: "Please choose your license?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "What is the contributing guideline of your project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please provide tests instructions of your project?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "please enter your Email address."
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter your GitHub Username."
+        },
+        {
+            type: "input",
+            name: "question",
+            message: "Please provide the insrtuctions on how to reach you."
+        },
+    ]);
+}
+// const questions = [
+    
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+//     return writeFileAsync(fileName, data);
+// }
 
 // function to initialize program
-function init() {
+async function init() {
+    console.log ("Please enter all the details for your project.")
+    try{
+        const answers = await promptUser();
+        const markdown = generateMarkdown(answers);
+        await writeFileAsync("readme.md" , markdown);
+        console.log("Success!");
+    } catch(err) {
+        console.log(err);
+    }
+} 
 
-}
 
+ 
 // function call to initialize program
 init();
---
+++ Develop/readme.md
@@ -0,0 +1,29 @@
+# readme generatot
+    ## Description
+    dfksdjfnsbd[fuhf
+    ## Table of Contents 
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    * [Contributing](#contributing)
+    * [GitHub](#github)
+    * [Email](#email) 
+    ## Installation
+    jsfjhfefhjewdkwefe
+    ## Usage Information
+    sfhfuiyewiwjgliu
+    ## License
+    undefined
+    ## Contributing Guidelines
+    hwufhifyewiurlkjfwbhvelfui;l
+    ## Tests Instructions
+    fuhieowycr8lmwoiurtnv
+    ## Email Address
+    miwnycot8pmu@gmail.com
+    ## GitHub Username
+    sfsfo
+    ### Contact Instructions
+    mfkjpfwe9aruwcer
